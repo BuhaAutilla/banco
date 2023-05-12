41908c94e47e3eb153d18c62c60f5604340f2a3a
Larysa Yanushchyk <buha.autilla@gmail.com>
2023-05-12 18:26:43 -0400

    H2 console le sirve

--- src/main/java/com/nttdata/banco/models/entities/Accounts.java
+++ src/main/java/com/nttdata/banco/models/entities/Accounts.java
@@ -15,7 +15,7 @@ import javax.persistence.Table;
 @Table(name = "accounts")
 public class Accounts implements Serializable{
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
     
     @Column(name = "name", length = 50, nullable = false)
